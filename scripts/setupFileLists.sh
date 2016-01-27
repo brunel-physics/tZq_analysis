3823eea7a53e52e5c23b16bd0bc6fa26610626e2
Alexander <alexander.david.morton@cern.ch>
2016-01-27 13:31:20 +0000

    Updated typo.

--- scripts/setupFileLists.sh
+++ scripts/setupFileLists.sh
@@ -41,21 +41,21 @@ ls /scratch/data/tZqSkimsRun2/emuRun2015D/* -1d >> $TQZ_TOOLS_PATH/configs/datas
 ls /scratch/data/tZqSkimsRun2/mumuRun2015C/* -1d >> $TQZ_TOOLS_PATH/configs/datasets/fileLists/mumuRun2015CFiles.txt
 ls /scratch/data/tZqSkimsRun2/mumuRun2015D/* -1d >> $TQZ_TOOLS_PATH/configs/datasets/fileLists/mumuRun2015DFiles.txt
 ls /scratch/data/tZqSkimsRun2/sChannel_4f/* -1d >> $TQZ_TOOLS_PATH/configs/datasets/fileLists/sChannelFiles.txt
-ls /scratch/data/tZqSkimsRun2/tZq_synchTest/* -1d  $TQZ_TOOLS_PATH/configs/datasets/fileLists/synchFiles.txt
-ls /scratch/data/tZqSkimsRun2/tbarChannel_4f/* -1d $TQZ_TOOLS_PATH/configs/datasets/fileLists/tbarChannelFiles.txt
-ls /scratch/data/tZqSkimsRun2/tW_antitop_5f/* -1d  $TQZ_TOOLS_PATH/configs/datasets/fileLists/tbarWInclusiveFiles.txt
+ls /scratch/data/tZqSkimsRun2/tZq_synchTest/* -1d  >> $TQZ_TOOLS_PATH/configs/datasets/fileLists/synchFiles.txt
+ls /scratch/data/tZqSkimsRun2/tbarChannel_4f/* -1d >> $TQZ_TOOLS_PATH/configs/datasets/fileLists/tbarChannelFiles.txt
+ls /scratch/data/tZqSkimsRun2/tW_antitop_5f/* -1d  >> $TQZ_TOOLS_PATH/configs/datasets/fileLists/tbarWInclusiveFiles.txt
 ls /scratch/data/tZqSkimsRun2/tChannel_4f/* -1d >> $TQZ_TOOLS_PATH/configs/datasets/fileLists/tChannelFiles.txt
 ls /scratch/data/tZqSkimsRun2/ttbarDilepton/* -1d >> $TQZ_TOOLS_PATH/configs/datasets/fileLists/ttbarDileptonFiles.txt
 ls /scratch/data/tZqSkimsRun2/ttW/* -1d >> $TQZ_TOOLS_PATH/configs/datasets/fileLists/ttWFiles.txt
 ls /scratch/data/tZqSkimsRun2/ttZ/* -1d >> $TQZ_TOOLS_PATH/configs/datasets/fileLists/ttZFiles.txt
 ls /scratch/data/tZqSkimsRun2/tW_top_5f/* -1d >> $TQZ_TOOLS_PATH/configs/datasets/fileLists/tWInclusiveFiles.txt
-ls /scratch/data/tZqSkims/tZq4Flavour3Lepton/* -1d >> $TQZ_TOOLS_PATH/configs/datasets/fileLists/tZqFilesRun1.txt
+ls /nfs/data/tZqSkims/tZq4Flavour3Lepton/* -1d >> $TQZ_TOOLS_PATH/configs/datasets/fileLists/tZqFilesRun1.txt
 ls /scratch/data/tZqSkimsRun2/tZq_ll_4Flavour3Lepton/* -1d >> $TQZ_TOOLS_PATH/configs/datasets/fileLists/tZqFiles.txt
 ls /scratch/data/tZqSkimsRun2/wPlusJets/* -1d >> $TQZ_TOOLS_PATH/configs/datasets/fileLists/wPlusJetsFiles.txt
 ls /scratch/data/tZqSkimsRun2/WW2l2nu/* -1d >> $TQZ_TOOLS_PATH/configs/datasets/fileLists/WW2l2nuFiles.txt
 ls /scratch/data/tZqSkimsRun2/WWinclusive* -1d >> $TQZ_TOOLS_PATH/configs/datasets/fileLists/WWinclusive.txt
 ls /scratch/data/tZqSkimsRun2/WZJets* -1d >> $TQZ_TOOLS_PATH/configs/datasets/fileLists/WZinclusive.txt
-ls /scratch/data/tZqSkimsRun2/ZZ2l2nu*-1d >> $TQZ_TOOLS_PATH/configs/datasets/fileLists/ZZ2l2nuFiles.txt
+ls /scratch/data/tZqSkimsRun2/ZZ2l2nu* -1d >> $TQZ_TOOLS_PATH/configs/datasets/fileLists/ZZ2l2nuFiles.txt
 ls /scratch/data/tZqSkimsRun2/ZZ2l2q/* -1d >> $TQZ_TOOLS_PATH/configs/datasets/fileLists/ZZ2l2qFiles.txt
 ls /scratch/data/tZqSkimsRun2/ZZ4l/* -1d >> $TQZ_TOOLS_PATH/configs/datasets/fileLists/ZZ4lFiles.txt
 
