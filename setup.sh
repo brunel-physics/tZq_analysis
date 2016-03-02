dc9734b42bc26405efc6ec6d85131d396fff7142
Alexander <alexander.david.morton@cern.ch>
2016-03-02 14:03:01 +0000

    Updated LHAPDF path in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${PWD}/lib:/home/eepgadm/lib/local/lib/"
-export LHAPATH="/cms/cmssw/slc6_amd64_gcc472/external/lhapdf/5.9.1/share/lhapdf/PDFsets/"
+#export LHAPATH="/cms/cmssw/slc6_amd64_gcc472/external/lhapdf/5.9.1/share/lhapdf/PDFsets/"
+export LHAPATH="/cms/cmssw/slc6_amd64_gcc493/external/lhapdf/6.1.5-kpegke3/share/LHAPDF/"
 
 export TQZ_TOOLS_PATH='.'
