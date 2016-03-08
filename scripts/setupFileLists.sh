3ef60e1aef9e84a057a7d12171070d658fdbd0c8
Alexander <alexander.david.morton@cern.ch>
2016-03-08 08:50:51 +0000

    Updated synch stuff - need to still update dataset configs.

--- scripts/setupFileLists.sh
+++ scripts/setupFileLists.sh
@@ -2,6 +2,8 @@
 echo Updating filelists for datasets used ...
 echo First deleting old filelists ...
 
+# Normal datasets
+
 rm $TQZ_TOOLS_PATH/configs/datasets/fileLists/DYJets10To50Files.txt
 rm $TQZ_TOOLS_PATH/configs/datasets/fileLists/DYJets50Files.txt
 rm $TQZ_TOOLS_PATH/configs/datasets/fileLists/eeRun2015CFiles.txt
@@ -11,7 +13,6 @@ rm $TQZ_TOOLS_PATH/configs/datasets/fileLists/emuRun2015DFiles.txt
 rm $TQZ_TOOLS_PATH/configs/datasets/fileLists/mumuRun2015CFiles.txt
 rm $TQZ_TOOLS_PATH/configs/datasets/fileLists/mumuRun2015DFiles.txt
 rm $TQZ_TOOLS_PATH/configs/datasets/fileLists/sChannelFiles.txt
-rm $TQZ_TOOLS_PATH/configs/datasets/fileLists/synchFiles.txt
 rm $TQZ_TOOLS_PATH/configs/datasets/fileLists/tbarChannelFiles.txt
 rm $TQZ_TOOLS_PATH/configs/datasets/fileLists/tbarWInclusiveFiles.txt
 rm $TQZ_TOOLS_PATH/configs/datasets/fileLists/tChannelFiles.txt
@@ -28,8 +29,18 @@ rm $TQZ_TOOLS_PATH/configs/datasets/fileLists/ZZ2l2nuFiles.txt
 rm $TQZ_TOOLS_PATH/configs/datasets/fileLists/ZZ2l2qFiles.txt
 rm $TQZ_TOOLS_PATH/configs/datasets/fileLists/ZZ4lFiles.txt
 
+# Synchronisation files
+
+rm $TQZ_TOOLS_PATH/configs/datasets/fileLists/tZqSynchFiles.txt
+rm $TQZ_TOOLS_PATH/configs/datasets/fileLists/WZsynchFiles.txt
+rm $TQZ_TOOLS_PATH/configs/datasets/fileLists/ttZsynchFiles.txt
+rm $TQZ_TOOLS_PATH/configs/datasets/fileLists/ttbarSynchFiles.txt
+
+
 echo Done!
 echo Now outputting the lists of the dataset directories into their relevant files ...
+
+# Normal datasets
 
 ls /scratch/data/tZqSkimsRun2/DYJetsToLL_M-10to50/* -1d >> $TQZ_TOOLS_PATH/configs/datasets/fileLists/DYJets10To50Files.txt
 ls /scratch/data/tZqSkimsRun2/DYJetsToLL_M-50/* -1d >> $TQZ_TOOLS_PATH/configs/datasets/fileLists/DYJets50Files.txt
@@ -40,7 +51,6 @@ ls /scratch/data/tZqSkimsRun2/emuRun2015D/* -1d >> $TQZ_TOOLS_PATH/configs/datas
 ls /scratch/data/tZqSkimsRun2/mumuRun2015C/* -1d >> $TQZ_TOOLS_PATH/configs/datasets/fileLists/mumuRun2015CFiles.txt
 ls /scratch/data/tZqSkimsRun2/mumuRun2015D/* -1d >> $TQZ_TOOLS_PATH/configs/datasets/fileLists/mumuRun2015DFiles.txt
 ls /scratch/data/tZqSkimsRun2/sChannel_4f/* -1d >> $TQZ_TOOLS_PATH/configs/datasets/fileLists/sChannelFiles.txt
-ls /scratch/data/tZqSkimsRun2/tZq_synchTest/* -1d  >> $TQZ_TOOLS_PATH/configs/datasets/fileLists/synchFiles.txt
 ls /scratch/data/tZqSkimsRun2/tbarChannel_4f/* -1d >> $TQZ_TOOLS_PATH/configs/datasets/fileLists/tbarChannelFiles.txt
 ls /scratch/data/tZqSkimsRun2/tW_antitop_5f/* -1d  >> $TQZ_TOOLS_PATH/configs/datasets/fileLists/tbarWInclusiveFiles.txt
 ls /scratch/data/tZqSkimsRun2/tChannel_4f/* -1d >> $TQZ_TOOLS_PATH/configs/datasets/fileLists/tChannelFiles.txt
@@ -56,6 +66,13 @@ ls /scratch/data/tZqSkimsRun2/WZJets/* -1d >> $TQZ_TOOLS_PATH/configs/datasets/f
 ls /scratch/data/tZqSkimsRun2/ZZ2l2nu/* -1d >> $TQZ_TOOLS_PATH/configs/datasets/fileLists/ZZ2l2nuFiles.txt
 ls /scratch/data/tZqSkimsRun2/ZZ2l2q/* -1d >> $TQZ_TOOLS_PATH/configs/datasets/fileLists/ZZ2l2qFiles.txt
 ls /scratch/data/tZqSkimsRun2/ZZ4l/* -1d >> $TQZ_TOOLS_PATH/configs/datasets/fileLists/ZZ4lFiles.txt
+
+# Synchronisation files
+
+ls /scratch/data/tZqSkimsRun2/synch/tZq* -1d  >> $TQZ_TOOLS_PATH/configs/datasets/fileLists/tZqSynchFiles.txt
+ls /scratch/data/tZqSkimsRun2/synch/WZ* -1d  >> $TQZ_TOOLS_PATH/configs/datasets/fileLists/WZsynchFiles.txt
+ls /scratch/data/tZqSkimsRun2/synch/ttZ* -1d  >> $TQZ_TOOLS_PATH/configs/datasets/fileLists/ttZsynchFiles.txt
+ls /scratch/data/tZqSkimsRun2/synch/ttbar* -1d  >> $TQZ_TOOLS_PATH/configs/datasets/fileLists/ttbarSynchFiles.txt
 
 echo Done!
 echo Filelists have been updated.
