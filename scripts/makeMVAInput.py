b2c11a7234018e55716faf3cfebd158830cc1718
Alexander <alexander.david.morton@cern.ch>
2016-04-19 14:35:57 +0100

    Updated jet SF path.

--- scripts/makeMVAInput.py
+++ scripts/makeMVAInput.py
@@ -382,7 +382,7 @@ def main():
 #    listOfMCs = {}
 
     #Set-up JEC corrections
-    jetUnc = JetCorrectionUncertainty("scaleFactors/Summer15_25nsV6M3_DATA_Uncertainty_AK4PFchs.txt")
+    jetUnc = JetCorrectionUncertainty("scaleFactors/Fall15_25nsV2_MC_Uncertainty_AK4PFchs.txt")
 
     #mapping of channels to dataTypes
     channelToDataset = {"eee":"DataEG","eemu":"DataMuEG","emumu":"DataMuEG","mumumu":"DataMu"}
