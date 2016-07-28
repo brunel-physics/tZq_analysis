df0efaf6eb7f5793e5c1201dc46fb25ce2b0220f
Alexander <alexander.david.morton@cern.ch>
2016-07-28 19:15:11 +0100

    Temp fix.

--- scripts/runDileptonAnalToBDT.py
+++ scripts/runDileptonAnalToBDT.py
@@ -19,8 +19,8 @@ mwStr = mwCut.split(".")[0]
 subprocess.call("mkdir mvaDirs/skims/mz"+mzStr+"mw"+mwStr,shell=True)
 
 for chanName in channelList.keys():
-    print "bin/analysisMain.exe -c configs/"+chanName+"MCdataConf.txt -u -t -k "+str(channelList[chanName])+" -v 16383 -z --dilepton --mvaDir mvaDirs/skims/mz"+mzStr+"mw"+mwStr + "/ --mzCut " + str(mzCut) + " --mwCut " + str(mwCut)
-    subprocess.call("bin/analysisMain.exe -c configs/"+chanName+"MCdataConf.txt -u -t -k "+str(channelList[chanName])+" -v 16383 -z --dilepton --mvaDir mvaDirs/skims/mz"+mzStr+"mw"+mwStr + "/ --mzCut " + str(mzCut) + " --mwCut " + str(mwCut),shell=True)
+    print "bin/analysisMain.exe -c configs/2015"+chanName+"MCdataConf.txt -u -t -k "+str(channelList[chanName])+" -v 16383 -z --dilepton --mvaDir mvaDirs/skims/mz"+mzStr+"mw"+mwStr + "/ --mzCut " + str(mzCut) + " --mwCut " + str(mwCut)
+    subprocess.call("bin/analysisMain.exe -c configs/2015/"+chanName+"MCdataConf.txt -u -t -k "+str(channelList[chanName])+" -v 16383 -z --dilepton --mvaDir mvaDirs/skims/mz"+mzStr+"mw"+mwStr + "/ --mzCut " + str(mzCut) + " --mwCut " + str(mwCut),shell=True)
 
 #Make the mvaInput directory
 subprocess.call("mkdir mvaDirs/inputs/mz"+mzStr+"mw"+mwStr,shell=True)
