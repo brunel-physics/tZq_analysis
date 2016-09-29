1612136423cb6fb7343ad8b4a4ad4b6b0e1bacfd
Alexander <alexander.david.morton@cern.ch>
2016-09-29 11:34:09 +0100

    Mini-debug.

--- scripts/makeMVAInputDilepton.py
+++ scripts/makeMVAInputDilepton.py
@@ -625,8 +625,9 @@ def main():
     listOfMCs = {"WW1l1nu2q" : "WW", "WW2l2nu":"WW","ZZ4l":"ZZ","ZZ2l2nu":"ZZ","ZZ2l2q":"ZZ","WZjets":"WZ","WZ2l2q":"WZ","WZ1l1nu2q":"WZ","sChannel":"TsChan","tChannel":"TtChan","tbarChannel":"TbartChan","tWInclusive":"TtW","tbarWInclusive":"TbartW","tZq":"tZq","tHq":"THQ","ttWlnu":"TTW","ttW2q":"TTW","ttZ2l2nu":"TTZ","ttZ2q":"TTZ","ttbarInclusivePowerheg":"TT","wPlusJets":"Wjets","DYJetsToLL_M-50":"DYToLL_M50","DYJetsToLL_M-10To50":"DYToLL_M10To50"}
 #    listOfMCs = {}
 
-    jetUnc = JetCorrectionUncertainty("../scaleFactors/2015/Fall15_25nsV2_MC_Uncertainty_AK4PFchs.txt")
-    #if (is2016) jetUnc = JetCorrectionUncertainty("Spring16_25nsV6_MC_Uncertainty_AK4PFchs.txt")
+    #jetUnc = JetCorrectionUncertainty("../scaleFactors/2015/Fall15_25nsV2_MC_Uncertainty_AK4PFchs.txt")
+    #if (is2016) 
+    jetUnc = JetCorrectionUncertainty("scaleFactors/2016/Spring16_25nsV6_MC_Uncertainty_AK4PFchs.txt")
 
     #mapping of channels to dataTypes
     channelToDataset = {"ee":"DataEG","mumu":"DataMu"}
