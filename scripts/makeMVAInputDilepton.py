83152fda85f008b2ac9de676f1ad257d6fd2ae44
Alexander <alexander.david.morton@cern.ch>
2016-12-01 15:22:47 +0000

    Ironed out bugs.

--- scripts/makeMVAInputDilepton.py
+++ scripts/makeMVAInputDilepton.py
@@ -51,7 +51,7 @@ def getJets(tree,syst,jetUnc,met,is2016):
     for i in range(15):
         if tree.jetInd[i] > -.5:
             jetList.append(tree.jetInd[i])
-            jetVecList.append(getJetVec(tree,tree.jetInd[i],met,is2016,True))
+            jetVecList.append(getJetVec(tree,tree.jetInd[i],met,is2016,syst,True))
         else: continue
     return (jetList,jetVecList)
 
@@ -62,16 +62,16 @@ def getBjets(tree,syst,jetUnc,met,jets,is2016):
     for i in range(10):
         if tree.bJetInd[i] > -0.5:
             bJetList.append(tree.bJetInd[i])
-            bJetVecList.append(getJetVec(tree,jets[tree.bJetInd[i]],met,is2016,False))
+            bJetVecList.append(getJetVec(tree,jets[tree.bJetInd[i]],met,is2016,syst,False))
         else:continue
 #    print len(bJetList)
     return (bJetList,bJetVecList)
 
-def getJetVec(tree, index, metVec, is2016, doMetSmear):
+def getJetVec(tree, index, metVec, is2016, syst, doMetSmear):
     #Gets a vector for a jet with corrections already applied
 
     newSmearValue = tree.jetSmearValue[index];
-    returnJet = = TLorentzVector();
+    returnJet = TLorentzVector();
 
     returnJet.SetPxPyPzE(newSmearValue*tree.jetPF2PATPx[index],newSmearValue*tree.jetPF2PATPy[index],newSmearValue*tree.jetPF2PATPz[index],newSmearValue*tree.jetPF2PATE[index]);
 
@@ -80,7 +80,7 @@ def getJetVec(tree, index, metVec, is2016, doMetSmear):
     elif syst == 32:
         returnJet *= 1+ jetUnc.getUncertainty(returnJet.Pt(), returnJet.Eta(),2)
 
-    if ( doMetSmear & newSmearValue > 0.01 ) :
+    if ( doMetSmear and newSmearValue > 0.01 ) :
     #Propogate through the met. But only do it if the smear jet isn't 0.
         metVec.SetPx(metVec.Px()+tree.jetPF2PATPx[index])
         metVec.SetPy(metVec.Py()+tree.jetPF2PATPy[index])
