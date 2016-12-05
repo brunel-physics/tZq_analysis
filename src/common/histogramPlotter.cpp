e5fc18614324aba7aa4f3a645362f66f35e2fb5b
Alexander <alexander.david.morton@cern.ch>
2016-12-05 15:45:13 +0000

    Update.

--- src/common/histogramPlotter.cpp
+++ src/common/histogramPlotter.cpp
@@ -14,7 +14,7 @@
 //For debugging. *sigh*
 #include <iostream>
 
-const bool BLIND_PLOTS( true ) ;
+const bool BLIND_PLOTS( true );
 
 HistogramPlotter::HistogramPlotter(std::vector<std::string> legOrder, std::vector<std::string> plotOrder, std::map<std::string,datasetInfo> dsetMap):
   //Initialise a load of variables. Labels are empty by default, but this can be changed by calling set label routines.
