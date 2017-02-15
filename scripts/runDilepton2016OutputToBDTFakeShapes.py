18d6ce478da18220d6ed950112340bd903a7a543
Alexander <alexander.david.morton@cern.ch>
2017-02-15 12:01:45 +0000

    Added standalone version of BDT input maker for fake shapes.

---
+++ scripts/runDilepton2016OutputToBDTFakeShapes.py
@@ -0,0 +1,24 @@
+#Quick script to loop over all channels to make fitting plots
+
+#from ROOT import *
+
+import subprocess
+import sys
+
+mzCut = sys.argv[1]
+mzStr = mzCut.split(".")[0]
+
+mwCut = sys.argv[2]
+mwStr = mwCut.split(".")[0]
+
+useSideBandRegion = ""
+dirExt = "all/"
+if len(sys.argv) > 3 and sys.argv[3] == "-s" :
+    useSideBandRegion = "-s"
+    dirExt = "sigCtrl/"
+
+#Make the mvaInput directory
+subprocess.call("mkdir /scratch/data/TopPhysics/mvaDirs/inputs/2016/"+dirExt+"mz"+mzStr+"mw"+mwStr,shell=True)
+
+print "python scripts/makeMVAInputDilepton_temp.py [\\\"ee\\\",\\\"mumu_part1\\\",\\\"mumu_part2\\\"] /scratch/data/TopPhysics/mvaDirs/skims/2016/mz"+mzStr+"mw"+mwStr+"/ /scratch/data/TopPhysics/mvaDirs/inputs/2016/"+dirExt+"mz"+mzStr+"mw"+mwStr+"/ --2016 "+useSideBandRegion
+subprocess.call("python scripts/makeMVAInputDilepton_temp.py [\\\"ee\\\",\\\"mumu_part1\\\",\\\"mumu_part2\\\"] /scratch/data/TopPhysics/mvaDirs/skims/2016/mz"+mzStr+"mw"+mwStr+"/ /scratch/data/TopPhysics/mvaDirs/inputs/2016/"+dirExt+"mz"+mzStr+"mw"+mwStr+"/ --2016 "+useSideBandRegion,shell=True)
