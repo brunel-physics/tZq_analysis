b7cf42a74b6c6348b7c132cfb484975090fbd2eb
Alexander <alexander.david.morton@cern.ch>
2017-04-01 13:47:47 +0100

    TTbar enriched control region mva input script - not used in MVA, but tools for reading values like yields read out in this format.

---
+++ scripts/runDilepton2016OutputToBDT_ttbar.py
@@ -0,0 +1,18 @@
+#Quick script to loop over all channels to make fitting plots
+
+#from ROOT import *
+
+import subprocess
+import sys
+
+useSideBandRegion = ""
+dirExt = "all/"
+if len(sys.argv) > 3 and sys.argv[3] == "-s" :
+    useSideBandRegion = "-s"
+    dirExt = "sigCtrl/"
+
+#Make the mvaInput directory
+subprocess.call("mkdir /scratch/data/TopPhysics/mvaDirs/inputs/2016/emu",shell=True)
+
+print "python scripts/makeMVAInputDilepton.py [\\\"emu\\\"] /scratch/data/TopPhysics/mvaDirs/skims/2016/emu/ /scratch/data/TopPhysics/mvaDirs/inputs/2016/emu/ --2016 "+useSideBandRegion
+subprocess.call("python scripts/makeMVAInputDilepton.py [\\\"emu\\\"] /scratch/data/TopPhysics/mvaDirs/skims/2016/emu/ /scratch/data/TopPhysics/mvaDirs/inputs/2016/emu/ --2016 "+useSideBandRegion,shell=True)
