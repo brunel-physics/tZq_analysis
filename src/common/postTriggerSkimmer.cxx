ca12947dbc74bd5229c5e321ea0085486fec2171
lunik1 <lunik1@users.noreply.github.com>
2018-07-24 18:41:05 +0100

    Do boolean logic at initialisation

--- src/common/postTriggerSkimmer.cxx
+++ src/common/postTriggerSkimmer.cxx
@@ -111,74 +111,86 @@ int main (int argc, char* argv[])
 	event.GetEntry(i);
 	
 	if ( channel == "ee" ) {
-	  bool eeTrig{false};
-	  if ( event.HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v3 > 0 ) eeTrig = true;
-	  if ( event.HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v4 > 0 ) eeTrig = true;
-	  if ( event.HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v5 > 0 ) eeTrig = true;
-	  if ( event.HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v6 > 0 ) eeTrig = true;
-	  if ( event.HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v7 > 0 ) eeTrig = true;
-	  if ( event.HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v8 > 0 ) eeTrig = true;
-	  if ( event.HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v9 > 0 ) eeTrig = true;
-	  
-	  if ( eeTrig ) {
-	    triggerDoubleCountCheck.emplace( std::make_pair(event.eventRun, event.eventNum) );
-	    outTree->Fill();
+        const bool eeTrig{
+            event.HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v3 > 0
+            || event.HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v4 > 0
+            || event.HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v5 > 0
+            || event.HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v6 > 0
+            || event.HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v7 > 0
+            || event.HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v8 > 0
+            || event.HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v9 > 0};
+
+        if (eeTrig)
+        {
+            triggerDoubleCountCheck.emplace(
+                std::make_pair(event.eventRun, event.eventNum));
+            outTree->Fill();
 	  }
 	}
 	
 	if ( channel == "mumu" ) {
-	  bool mumuTrig{false};
-	  if ( event.HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_v2 > 0 ) mumuTrig = true;
-	  if ( event.HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_v3 > 0 ) mumuTrig = true;
-	  if ( event.HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_v4 > 0 ) mumuTrig = true;
-	  if ( event.HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_v5 > 0 ) mumuTrig = true;
-	  if ( event.HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_v6 > 0 ) mumuTrig = true;
-	  if ( event.HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_v7 > 0 ) mumuTrig = true;
-	  if ( event.HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ_v2 > 0 ) mumuTrig = true;
-	  if ( event.HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ_v3 > 0 ) mumuTrig = true;
-	  if ( event.HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ_v4 > 0 ) mumuTrig = true;
-	  if ( event.HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ_v5 > 0 ) mumuTrig = true;
-	  if ( event.HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ_v6 > 0 ) mumuTrig = true;
-	  
-	  if ( mumuTrig ) {
-	    triggerDoubleCountCheck.emplace( std::make_pair(event.eventRun, event.eventNum) );
-	    outTree->Fill();
+        const bool mumuTrig{
+            event.HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_v2 > 0
+            || event.HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_v3 > 0
+            || event.HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_v4 > 0
+            || event.HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_v5 > 0
+            || event.HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_v6 > 0
+            || event.HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_v7 > 0
+            || event.HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ_v2 > 0
+            || event.HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ_v3 > 0
+            || event.HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ_v4 > 0
+            || event.HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ_v5 > 0
+            || event.HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ_v6 > 0};
+
+        if (mumuTrig)
+        {
+            triggerDoubleCountCheck.emplace(
+                std::make_pair(event.eventRun, event.eventNum));
+            outTree->Fill();
 	  }
 	}
 	
 	if ( channel == "emu" ) {
-	  bool muEGTrig{false};
-	  // Runs B-G only
-	  if ( event.eventRun < 280919 ) {
-	    if ( event.HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v3 > 0 ) muEGTrig = true;
-	    if ( event.HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v4 > 0 ) muEGTrig = true;
-	    if ( event.HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v5 > 0 ) muEGTrig = true;
-	    if ( event.HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v6 > 0 ) muEGTrig = true;
-	    if ( event.HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v7 > 0 ) muEGTrig = true;
-	    if ( event.HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v8 > 0 ) muEGTrig = true;
-	    if ( event.HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v9 > 0 ) muEGTrig = true;
-	    if ( event.HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v3 > 0 ) muEGTrig = true;
-	    if ( event.HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v4 > 0 ) muEGTrig = true;
-	    if ( event.HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v5 > 0 ) muEGTrig = true;
-	    if ( event.HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v6 > 0 ) muEGTrig = true;
-	    if ( event.HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v7 > 0 ) muEGTrig = true;
-	    if ( event.HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v8 > 0 ) muEGTrig = true;
-	    if ( event.HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v9 > 0 ) muEGTrig = true;
-	  }
-	  // Run H only
-	  if ( event.eventRun >= 280919 ) {
-	    if ( event.HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v1 > 0 ) muEGTrig = true;
-	    if ( event.HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v2 > 0 ) muEGTrig = true;
-	    if ( event.HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v3 > 0 ) muEGTrig = true;
-	    if ( event.HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v4 > 0 ) muEGTrig = true;
-	    if ( event.HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v1 > 0 ) muEGTrig = true;
-	    if ( event.HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v2 > 0 ) muEGTrig = true;
-	    if ( event.HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v3 > 0 ) muEGTrig = true;
-	    if ( event.HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v4 > 0 ) muEGTrig = true;
-	  }
-	  if ( muEGTrig ) {
-	    triggerDoubleCountCheck.emplace( std::make_pair(event.eventRun, event.eventNum) );
-	    outTree->Fill();
+        const bool muEGTrig{
+            (event.eventRun < 280919
+             && (event.HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v3 > 0
+                 || event.HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v4 > 0
+                 || event.HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v5 > 0
+                 || event.HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v6 > 0
+                 || event.HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v7 > 0
+                 || event.HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v8 > 0
+                 || event.HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v9 > 0
+                 || event.HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v3 > 0
+                 || event.HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v4 > 0
+                 || event.HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v5 > 0
+                 || event.HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v6 > 0
+                 || event.HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v7 > 0
+                 || event.HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v8 > 0
+                 || event.HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v9
+                        > 0))
+            || (event.eventRun >= 280919
+                && (event.HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v1
+                        > 0
+                    || event.HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v2
+                           > 0
+                    || event.HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v3
+                           > 0
+                    || event.HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v4
+                           > 0
+                    || event.HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v1
+                           > 0
+                    || event.HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v2
+                           > 0
+                    || event.HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v3
+                           > 0
+                    || event.HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v4
+                           > 0))};
+
+        if (muEGTrig)
+        {
+            triggerDoubleCountCheck.emplace(
+                std::make_pair(event.eventRun, event.eventNum));
+            outTree->Fill();
 	  }
 	} // end emu	
       }
@@ -229,16 +241,16 @@ int main (int argc, char* argv[])
 	++progress;  // update progress bar (++ must be prefix)
 	event.GetEntry(i);
 	if ( channel == "ee" ) {
-          bool eTrig{false};
-	  if ( event.HLT_Ele32_eta2p1_WPTight_Gsf_v2 > 0 ) eTrig = true;
-	  if ( event.HLT_Ele32_eta2p1_WPTight_Gsf_v3 > 0 ) eTrig = true;
-	  if ( event.HLT_Ele32_eta2p1_WPTight_Gsf_v4 > 0 ) eTrig = true;
-	  if ( event.HLT_Ele32_eta2p1_WPTight_Gsf_v5 > 0 ) eTrig = true;
-	  if ( event.HLT_Ele32_eta2p1_WPTight_Gsf_v6 > 0 ) eTrig = true;
-	  if ( event.HLT_Ele32_eta2p1_WPTight_Gsf_v7 > 0 ) eTrig = true;
-	  if ( event.HLT_Ele32_eta2p1_WPTight_Gsf_v8 > 0 ) eTrig = true;
+        const bool eTrig{event.HLT_Ele32_eta2p1_WPTight_Gsf_v2 > 0
+                         || event.HLT_Ele32_eta2p1_WPTight_Gsf_v3 > 0
+                         || event.HLT_Ele32_eta2p1_WPTight_Gsf_v4 > 0
+                         || event.HLT_Ele32_eta2p1_WPTight_Gsf_v5 > 0
+                         || event.HLT_Ele32_eta2p1_WPTight_Gsf_v6 > 0
+                         || event.HLT_Ele32_eta2p1_WPTight_Gsf_v7 > 0
+                         || event.HLT_Ele32_eta2p1_WPTight_Gsf_v8 > 0};
 
-	  if ( eTrig ) {
+        if (eTrig)
+        {
             auto it = triggerDoubleCountCheck.find( std::make_pair( event.eventRun, event.eventNum ) );
 	    singleElectron++;
 	    // If event has already been found ... skip event
@@ -252,19 +264,17 @@ int main (int argc, char* argv[])
 	} // end single electron check for ee
 	
 	if ( channel == "mumu") {
-	  bool muTrig{false};
-	  if ( event.HLT_IsoMu24_v1 > 0 ) muTrig = true;
-	  if ( event.HLT_IsoMu24_v2 > 0 ) muTrig = true;
-	  if ( event.HLT_IsoMu24_v3 > 0 ) muTrig = true;
-	  if ( event.HLT_IsoMu24_v4 > 0 ) muTrig = true;
-	  if ( event.HLT_IsoTkMu24_v1 > 0 ) muTrig = true;
-	  if ( event.HLT_IsoTkMu24_v2 > 0 ) muTrig = true;
-	  if ( event.HLT_IsoTkMu24_v3 > 0 ) muTrig = true;
-	  if ( event.HLT_IsoTkMu24_v4 > 0 ) muTrig = true;
+        const bool muTrig{
+            event.HLT_IsoMu24_v1 > 0 || event.HLT_IsoMu24_v2 > 0
+            || event.HLT_IsoMu24_v3 > 0 || event.HLT_IsoMu24_v4 > 0
+            || event.HLT_IsoTkMu24_v1 > 0 || event.HLT_IsoTkMu24_v2 > 0
+            || event.HLT_IsoTkMu24_v3 > 0 || event.HLT_IsoTkMu24_v4 > 0};
 
-	  // If single Muon triggered fired, check to see if event also fired a DoubleMuon trigger
-	  if ( muTrig ) {
-	    singleMuon++;
+        // If single Muon triggered fired, check to see if event also fired a
+        // DoubleMuon trigger
+        if (muTrig)
+        {
+            singleMuon++;
             auto it = triggerDoubleCountCheck.find( std::make_pair( event.eventRun, event.eventNum ) );
 	    // If event has already been found ... skip event
 	    if ( it != triggerDoubleCountCheck.end() ) dupMuon++;
@@ -279,26 +289,21 @@ int main (int argc, char* argv[])
 	
 	if ( channel == "emu" ) {
 	  // check eTrigger for emu first
-          bool eTrig{false};
-	  if ( event.HLT_Ele32_eta2p1_WPTight_Gsf_v2 > 0 ) eTrig = true;
-	  if ( event.HLT_Ele32_eta2p1_WPTight_Gsf_v3 > 0 ) eTrig = true;
-	  if ( event.HLT_Ele32_eta2p1_WPTight_Gsf_v4 > 0 ) eTrig = true;
-	  if ( event.HLT_Ele32_eta2p1_WPTight_Gsf_v5 > 0 ) eTrig = true;
-	  if ( event.HLT_Ele32_eta2p1_WPTight_Gsf_v6 > 0 ) eTrig = true;
-	  if ( event.HLT_Ele32_eta2p1_WPTight_Gsf_v7 > 0 ) eTrig = true;
-	  if ( event.HLT_Ele32_eta2p1_WPTight_Gsf_v8 > 0 ) eTrig = true;
-	  // then check muTrigger for emu
-	  bool muTrig{false};
-	  if ( event.HLT_IsoMu24_v1 > 0 ) muTrig = true;
-	  if ( event.HLT_IsoMu24_v2 > 0 ) muTrig = true;
-	  if ( event.HLT_IsoMu24_v3 > 0 ) muTrig = true;
-	  if ( event.HLT_IsoMu24_v4 > 0 ) muTrig = true;
-	  if ( event.HLT_IsoTkMu24_v1 > 0 ) muTrig = true;
-	  if ( event.HLT_IsoTkMu24_v2 > 0 ) muTrig = true;
-	  if ( event.HLT_IsoTkMu24_v3 > 0 ) muTrig = true;
-	  if ( event.HLT_IsoTkMu24_v4 > 0 ) muTrig = true;
+      const bool eTrig{event.HLT_Ele32_eta2p1_WPTight_Gsf_v2 > 0
+                       || event.HLT_Ele32_eta2p1_WPTight_Gsf_v3 > 0
+                       || event.HLT_Ele32_eta2p1_WPTight_Gsf_v4 > 0
+                       || event.HLT_Ele32_eta2p1_WPTight_Gsf_v5 > 0
+                       || event.HLT_Ele32_eta2p1_WPTight_Gsf_v6 > 0
+                       || event.HLT_Ele32_eta2p1_WPTight_Gsf_v7 > 0
+                       || event.HLT_Ele32_eta2p1_WPTight_Gsf_v8 > 0};
+      // then check muTrigger for emu
+      const bool muTrig{
+          event.HLT_IsoMu24_v1 > 0 || event.HLT_IsoMu24_v2 > 0
+          || event.HLT_IsoMu24_v3 > 0 || event.HLT_IsoMu24_v4 > 0
+          || event.HLT_IsoTkMu24_v1 > 0 || event.HLT_IsoTkMu24_v2 > 0
+          || event.HLT_IsoTkMu24_v3 > 0 || event.HLT_IsoTkMu24_v4 > 0};
 
-	  // If either single lepton  triggered fired, check to see if event also fired a MuonEG trigger
+      // If either single lepton  triggered fired, check to see if event also fired a MuonEG trigger
 	  if ( eTrig || muTrig ) {
             if ( eTrig ) singleElectron++;
             if ( muTrig ) singleMuon++;
