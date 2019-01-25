8331dd376bedc312a5de30174091ce47c2ad66bf
lunik1 <lunik1@users.noreply.github.com>
2019-01-25 13:22:30 +0000

    Make the max file size effectively unlimited

--- src/analysisMain.cxx
+++ src/analysisMain.cxx
@@ -1,10 +1,15 @@
 #include "analysisAlgo.hpp"
 
+#include "TTree.h"
+
 #include <iomanip>
 #include <iostream>
+#include <limits>
 
 int main(int argc, char* argv[])
 {
+    TTree::SetMaxTreeSize(std::numeric_limits<long long>::max());
+
     AnalysisAlgo analysisMain;
 
     analysisMain.parseCommandLineArguements(argc, argv);
