2ef5f279fbaa33432ef70544ff23abf9d11e84e2
lunik1 <lunik1@users.noreply.github.com>
2019-02-19 15:47:47 +0000

    Add LCG_95 to setup script

--- setup.sh
+++ setup.sh
@@ -4,9 +4,4 @@ export LHAPATH=/cvmfs/cms.cern.ch/lhapdf/pdfsets/6.1.6/
 export TQZ_TOOLS_PATH='.'
 
 # Enable newrt gcc and python
-source /scratch/shared/sw/gcc/x86_64-slc6-gcc62-opt/setup.sh
-export PATH=/scratch/shared/sw/Python/2.7.4/x86_64-slc6-gcc48-opt/bin:$PATH
-export PYTHONDIR=/scratch/shared/sw/Python/2.7.4/x86_64-slc6-gcc48-opt
-export PYTHONPATH=/scratch/shared/sw/Python/2.7.4/x86_64-slc6-gcc48-opt
-export PYTHONPATH=$PYTHONPATH:$ROOTSYS/lib
-export LD_LIBRARY_PATH=$ROOTSYS/lib:$PYTHONDIR/lib:$LD_LIBRARY_PATH
+source /cvmfs/sft.cern.ch/lcg/views/setupViews.sh LCG_95 x86_64-slc6-gcc8-opt 
